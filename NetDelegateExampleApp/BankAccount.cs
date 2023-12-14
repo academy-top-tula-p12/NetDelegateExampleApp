63ac87f140c4727fb4faa6042c91942f3e51097d
maxefimov <efimov.top.academy@gmail.com>
2023-12-14 21:07:41 +0300

    Добавьте файлы проекта.

---
+++ NetDelegateExampleApp/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetDelegateExampleApp
+{
+    delegate void AccountHandler(string message);
+
+    class BankAccount
+    {
+        int amount;
+
+        AccountHandler? handler;
+
+        public BankAccount(int amount = 0) 
+        {
+            this.amount = amount;
+        }
+
+        public void AddMoney(int amount) => this.amount += amount;
+
+        public void TakeMoney(int amount)
+        {
+            if (amount <= this.amount)
+            {
+                this.amount -= amount;
+                handler?.Invoke($"from account takes {amount} rub.");
+            }
+            else
+                handler?.Invoke($"Balance of account {this.amount} less than {amount}");
+        }
+
+        public void RegisterAccountHandler(AccountHandler handler) 
+            => this.handler += handler;
+
+        public void UnregisterAccountHandler(AccountHandler handler)
+            => this.handler -= handler;
+
+    }
+}
---
+++ NetDelegateExampleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using NetDelegateExampleApp;
+using System.Threading.Channels;
+
+BankAccount account = new BankAccount(1000);
+account.RegisterAccountHandler(PrintConsoleMessage);
+account.RegisterAccountHandler(PrintRedConsoleMessage);
+
+account.TakeMoney(300);
+account.TakeMoney(500);
+
+account.UnregisterAccountHandler(PrintConsoleMessage);
+
+account.TakeMoney(300);
+
+
+
+
+void PrintConsoleMessage(string message) => Console.WriteLine(message);
+
+void PrintRedConsoleMessage(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(message);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+    
